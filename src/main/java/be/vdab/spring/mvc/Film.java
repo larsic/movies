06536c72d8f613aaaa14e9aeb26681cb418446e1
larsic <larsvk@gmail.com>
2016-09-20 15:41:42 +0200

    form fields validation check

--- src/main/java/be/vdab/spring/mvc/Film.java
+++ src/main/java/be/vdab/spring/mvc/Film.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Film {
@@ -11,9 +15,24 @@ public class Film {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
+    @Size(min=2)
     private String title;
+
+    @NotNull
+    @Size(min=2)
     private String description;
+
+    @NotNull
+    @Size(min=4, max=4)
+    @Max(2020)
+    @Min(1850)
     private String releaseYear;
+
+    @NotNull
+
+    @Max(500)
+    @Min(1)
     private int length;
 
     public Film() {
